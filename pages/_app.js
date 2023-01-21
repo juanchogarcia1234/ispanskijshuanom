01c1f9ce748f5dbd782774971b82460dc3a641b3
Juan SME <juansme@Juans-MacBook-Pro.local>
2023-01-21 15:10:44 +0100

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/index.css";
 import { ApolloProvider } from "@apollo/client/react";
 import { client } from "../lib/apollo";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
