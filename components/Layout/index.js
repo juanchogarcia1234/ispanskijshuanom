f1ac0e71dd38f22462431d8650c8fdd7dc35ba0b
Juan SME <juansme@Juans-MacBook-Pro.local>
2023-02-10 23:47:56 +0100

    add Adsense

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -17,6 +17,11 @@ export default function Layout({ children }) {
 				<link rel="icon" href="favicon.ico"></link>
 				<meta name="robots" content="noindex"></meta>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+				<script
+					async
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2356493060345810"
+					crossorigin="anonymous"
+				></script>
 			</Head>
 			<Header />
 			<main className={manrope.className}>{children}</main>
